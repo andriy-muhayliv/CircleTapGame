9c15ee068de09c79b33effb9a72392fcf6076c6f
gyd3ukc <70544293+gyd3ukc@users.noreply.github.com>
2020-12-28 20:04:41 +0200

    Update darkMode.js

--- js/darkMode.js
+++ js/darkMode.js
@@ -2,15 +2,14 @@ const darkMode = document.querySelector('#darkMode');
 const body = document.querySelector('body');
 
 darkMode.onclick = function() {
-if (darkMode.textContent == 'Dark mode'){
 navigator.vibrate(20);
+if (darkMode.textContent == 'Dark mode'){
     body.style.backgroundColor = 'black';
 darkMode.textContent = 'White mode';
 for (let i = 0; i < body.children.length; i++){
 body.children[i].style.color = 'white';
         } 
 } else {
-navigator.vibrate(20);
         body.style.backgroundColor = 'white';
     darkMode.textContent = 'Dark mode';
     for (let i = 0; i < body.children.length; i++){
